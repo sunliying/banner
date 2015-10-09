691bb10f3d8e4294420f893f48f5eb13b4be0ccc
sunliying <1174587076@qq.com>
2015-10-09 20:02:42 +0800

    勉强功能都实现了

--- js/banner.js
+++ js/banner.js
@@ -11,72 +11,97 @@ var warpper = (function(){
 	    cuts=document.querySelectorAll(".swhich"),
 	    picts=document.querySelectorAll(".banner .image"),
 	    lists=document.querySelectorAll(".banner .tag li");
+	var change=true;
 	picts[count].className+=" image_current ";
 	lists[count].style.backgroundColor=("blue");
 	//设置初始化
 	function reset(){
 		for (var i = 0; i < picts.length; i++) {
-			picts[i].className="image"
-			lists[i].style.backgroundColor=("#fff");
-		};
+			picts[i].className="image";
+			lists[i].style.backgroundColor=("#fff");	                                                
+	}
+	}
+	//在动画进行时加入了一个classname，用来控制按钮
+    for(var i=0; i<picts.length;i++){  //直接用i的话不行，必须传入参数，好像i不是固定的
+    	(function(j){
+    		picts[j].addEventListener("webkitAnimationStart",function(){
+				picts[j].className+=" ison ";
+			}) ; 
+			picts[j].addEventListener("webkitAnimationEnd",function(){
+				picts[j].className=picts[j].className.replace(/ison/," ");
+			}) ;
+    	})(i)
+    	      
+		manaul(lists[i],picts[i],i);
+
+	}
+	//手动操作
+	function manaul (list,pic,j){
+		list.addEventListener("click",function(){
+
+			if(!(/ison/.test(picts[count].className))){
+				window.clearInterval(init);
+    			reset();
+    			list.style.backgroundColor=("blue");
+			    if(j>count){
+					picts[count].className+=" image_current_next ";
+					pic.className+=" image_next ";	
+				}
+				else if(j<count){
+					picts[count].className+=" image_current_pre ";
+					pic.className+=" image_pre ";	
+				}else{
+				picts[count].className+=" image_current ";
+				lists[count].style.backgroundColor=("blue");	
+				}
+				count=j;
+				init=window.setInterval(auto,5000);
+    		}
+		})
 	}
 	//设置自动切换
 	function auto(){
-		reset();
-        picts[count].className+=" image_current_next ";
-        if(count!=picts.length-1){
-        	count++;
-        }else{
-        	count=0;
-        }
-        picts[count].className+=" image_next ";
-        lists[count].style.backgroundColor=("blue");
-	}
-	init=window.setInterval(auto,3000);
-	//左右两侧的切换
-	cuts[0].addEventListener("click",function(){ 
-    		window.clearTimeout(init);
-    		reset();
-			picts[count].className+=" image_current_pre ";
-            if(count==0)
-            	{count=lists.length-1;}
-        	else{ count--;}
-        	picts[count].className+=" image_pre ";
-            lists[count].style.backgroundColor=("blue");
-    	})
-    cuts[1].addEventListener("click",function(){ 
-    		window.clearTimeout(init);
-    	    reset();
-            picts[count].className+=" image_current_next ";
-            if(count==lists.length-1)
-            	{count=0;}
-        	else{ count++;}
-        	picts[count].className+=" image_next ";
-            lists[count].style.backgroundColor=("blue");
-    	})
-    //手动操作
-    for(var i=0; i<lists.length;i++){
-		manaul(lists[i],picts[i],i);
-	}
-	function manaul (list,pic,j){
-		list.addEventListener("click",function(){
-			window.clearTimeout(init);
+		if(!(/ison/.test(picts[count].className))){
 			reset();
-		    list.style.backgroundColor=("blue");
-		    if(j>count){
-				picts[count].className+=" image_current_next ";
-				pic.className+=" image_next ";	
-			}
-			else if(j<count){
-				picts[count].className+=" image_current_pre ";
-				pic.className+=" image_pre ";	
-			}else{
-			picts[count].className+=" image_current ";
-			lists[count].style.backgroundColor=("blue");	
-			}
-			count=j;
-		})
+	        picts[count].className+=" image_current_next ";
+	        if(++count == picts.length){
+	        	count=0;
+	        }
+	        picts[count].className+=" image_next ";
+	        lists[count].style.backgroundColor=("blue");
+	       }
 	}
+	init=window.setInterval(auto,5000);
+	//左右两侧的切换
+	
+	cuts[0].addEventListener("click",function(){ 
+		if(!(/ison/.test(picts[count].className))){
+				window.clearInterval(init);
+    			reset();
+    			picts[count].className+=" image_current_pre ";
+	            if(--count==-1)
+	            	{count=lists.length-1;}
+	        	picts[count].className+=" image_pre ";
+	            lists[count].style.backgroundColor=("blue");
+	            init=window.setInterval(auto,5000);
+    	}
+
+	})
+	
+    cuts[1].addEventListener("click",function(){ 
+    	if(!(/ison/.test(picts[count].className))){
+    		window.clearInterval(init);
+    	    reset();
+    	    
+    			picts[count].className+=" image_current_next ";
+	            if(++count==lists.length)
+	            	{count=0;}
+	        	picts[count].className+=" image_next ";
+	            lists[count].style.backgroundColor=("blue");
+				init=window.setInterval(auto,5000);    		
+          }  
+    	})
+    
 	//清空自动切换
 	function clear(){
 		window.clearTimeout(init);
